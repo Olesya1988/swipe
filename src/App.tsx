0fe2efcdd5e13d44967d40879acac5b1d03aefec
Olesya Vetoshkina <our_post-office@mail.ru>
2023-11-02 22:17:25 +0300

    original

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { SwipeGallery } from "./components/SwipeGallery";
 
 const images = [
-  { url: "../public/images/1.jpg", description: "SP 1" },
-  { url: "../public/images/2.jpg", description: "SP 2" },
-  { url: "../public/images/3.jpg", description: "SP 3" },
-  { url: "../public/images/4.jpg", description: "SP 4" },
-  { url: "../public/images/5.jpg", description: "SP 5" },
-  { url: "../public/images/6.jpg", description: "SP 6" },
+  { url: "./images/1.jpg", description: "SP 1" },
+  { url: "./images/2.jpg", description: "SP 2" },
+  { url: "./images/3.jpg", description: "SP 3" },
+  { url: "./images/4.jpg", description: "SP 4" },
+  { url: "./images/5.jpg", description: "SP 5" },
+  { url: "./images/6.jpg", description: "SP 6" },
 ];
 
 function App() {
