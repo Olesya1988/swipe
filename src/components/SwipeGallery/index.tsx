52b0666336b88502dc0f09e604f03bee5b313527
Olesya Vetoshkina <our_post-office@mail.ru>
2023-11-02 17:44:54 +0300

    original

--- src/components/SwipeGallery/index.tsx
+++ src/components/SwipeGallery/index.tsx
@@ -4,14 +4,13 @@ interface SwipeGalleryProps {
   images: {
     url: string;
     description: string;
-  };
+  }[]
 }
-[];
 
 export const SwipeGallery = ({ images }: SwipeGalleryProps) => (
   <div className="gallery">
     <div className="gallery-container">
-      {images.map((image: any, index: any) => (
+      {images.map((image, index) => (
         <div key={index} className="gallery-container-item">
           <img src={image.url} alt={image.description} loading="lazy" />
         </div>
